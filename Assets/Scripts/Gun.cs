5fb6e4a2f2d5a25055d21b4a01fa06f768696b7e
Felix <984082450@qq.com>
2019-01-14 00:50:41 +0800

    Weapon variation.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -12,7 +12,7 @@ public class Gun : MonoBehaviour
 		Count
 	}
 
-	public Transform muzzlePoint; //枪口
+	public Transform[] muzzleFirePoints; //枪口火力点
 	public ParticleSystem fireEfx;
 	public Light fireEfxLight;
 	public Projectile projectile;
@@ -52,12 +52,15 @@ public class Gun : MonoBehaviour
 				}
 			}
 
-			nextShootTime = Time.time + timeBtwShoot / 1000f;
-			Projectile shootProjectile = Instantiate(this.projectile) as Projectile;
-			shootProjectile.transform.SetParent(muzzlePoint, false);
-			shootProjectile.transform.localPosition = new Vector3(Random.Range(-0.08f, 0.08f), 0, 0);
-			shootProjectile.transform.SetParent(null, true);
-			shootProjectile.SetSpeed(muzzleVelocity);
+			for (int i = 0; i < muzzleFirePoints.Length; i++)
+			{
+				nextShootTime = Time.time + timeBtwShoot / 1000f;
+				Projectile shootProjectile = Instantiate(this.projectile) as Projectile;
+				shootProjectile.transform.SetParent(muzzleFirePoints[i], false);
+				shootProjectile.transform.localPosition = new Vector3(Random.Range(-0.08f, 0.08f), 0, 0);
+				shootProjectile.transform.SetParent(null, true);
+				shootProjectile.SetSpeed(muzzleVelocity);
+			}
 
 			fireEfx.Play();
 
